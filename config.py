7687a8f8e9e321b1aa5412fffc58b0a290c7b3b9
Blaine Quackenbush <blaine.quackenbush@gmail.com>
2023-02-20 18:38:42 -0800

    update configs

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ params = dict(state_dim=448,
               easy_mode=2309,
               easy_guess=2309,
               batch_size=500,
-              actor_lr=1e-5,
-              critic_lr=1e-4,
+              actor_lr=1e-4,
+              critic_lr=1e-3,
               gamma=0.2,
-              parallel_workers=5)
+              parallel_workers=6)
