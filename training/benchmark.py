e17c392ad7b48baab8c4889f0be6ed1b18ba7e3a
bquacken <68025393+bquacken@users.noreply.github.com>
2023-03-16 11:22:02 -0700

    show example game

--- training/benchmark.py
+++ training/benchmark.py
@@ -11,6 +11,8 @@ def benchmark_model(actor: nn.Module, answers):
     wins = 0
     scores = []
     actor.eval()
+    rand = np.random.randint(0, len(answers))
+    i = 0
     for ans in tqdm(answers):
         env.reset(ans)
         while not env.wordle.over:
@@ -20,6 +22,11 @@ def benchmark_model(actor: nn.Module, answers):
         if env.wordle.win:
             wins += 1
             scores.append(env.num_guesses)
+        if i == rand:
+            print('Answer: ',env.wordle.answer)
+            print('Guesses: ',env.wordle.guesses)
+
+        i += 1
     avg_score = np.mean(scores)
     print(f'Total Wins out of {len(answers)} Games: {wins}')
     print(f'Average Number of Guesses per Win: {avg_score:02f}')
