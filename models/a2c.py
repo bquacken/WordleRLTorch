772fcfe6684dd96095e4c6aa31456e4d4b514e44
Blaine Quackenbush <blaine.quackenbush@gmail.com>
2023-03-20 09:42:14 -0700

    lr schedule, feed forward size

--- models/a2c.py
+++ models/a2c.py
@@ -10,6 +10,13 @@ else:
     device = torch.device('cpu')
 
 
+def mlp_lr(epoch):
+    if epoch < 4000:
+        return 1
+    else:
+        return 1
+
+
 def compute_advantages(rewards: torch.Tensor, values: torch.Tensor, dones: torch.Tensor):
     discount_factor = 0.99
     with torch.no_grad():
@@ -23,7 +30,6 @@ def compute_advantages(rewards: torch.Tensor, values: torch.Tensor, dones: torch
         return returns, advantages
 
 
-
 class ActorCritic(nn.Module):
     def __init__(self, mode: str = 'hard', dev: str = 'cpu'):
         super().__init__()
@@ -32,7 +38,7 @@ class ActorCritic(nn.Module):
         self.device = torch.device(dev)
 
         self.d_input = params['state_dim']
-        self.d_embed = 256
+        self.d_embed = 512
         self.head = nn.Sequential(nn.Linear(self.d_input, self.d_embed), nn.Tanh(),
                                   nn.Linear(self.d_embed, self.d_embed), nn.Tanh(),
                                   nn.Linear(self.d_embed, self.d_embed), nn.Tanh())
@@ -54,7 +60,8 @@ class ActorCritic(nn.Module):
             self.head.cuda()
             self.value.cuda()
             self.policy.cuda()
-        self.optim = torch.optim.RMSprop(self.parameters(), lr=params['lr'])
+        self.optim = torch.optim.RMSprop(self.parameters(), lr=params['mlp_lr'])
+        self.scheduler = torch.optim.lr_scheduler.LambdaLR(self.optim, mlp_lr)
 
     def forward(self, x):
         x = self.head(x)
@@ -75,7 +82,6 @@ class ActorCritic(nn.Module):
         dones = torch.tensor(memory.dones, dtype=torch.int8).to(self.device)
         rewards = torch.tensor(memory.rewards, dtype=torch.int8).to(self.device)
 
-
         returns, advantages = compute_advantages(rewards, values, dones)
         acts_advs = torch.zeros((len(actions), 2))
         acts_advs[:, 0] = actions
@@ -88,4 +94,5 @@ class ActorCritic(nn.Module):
         loss = actor_loss + critic_loss
         loss.backward()
         self.optim.step()
+        self.scheduler.step()
         return actor_loss.detach().cpu().numpy(), critic_loss.detach().cpu().numpy()
