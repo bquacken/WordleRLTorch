1841604276af154a930c70469248da5e4fa3288b
Blaine Quackenbush <blaine.quackenbush@gmail.com>
2023-04-03 07:26:17 -0700

    Fixed logarithm bug

--- training/losses.py
+++ training/losses.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torch.nn.modules.loss import _Loss
 from config import params
 
 if torch.cuda.is_available():
@@ -17,16 +16,17 @@ def critic_loss_fn(returns: torch.Tensor, values: torch.Tensor) -> torch.Tensor:
 
 
 def actor_loss_fn(actions_advantages: torch.Tensor,
-                  policy_logits: torch.Tensor, char_logits) -> torch.Tensor:
+                  policy_logits: torch.Tensor, char_logits) -> tuple[torch.Tensor, torch.Tensor]:
     actions = actions_advantages[:, 0].type(torch.LongTensor).to(device)
     advantages = actions_advantages[:, 1].to(device)
     probs = nn.Softmax(dim=1)(policy_logits).to(device)
-    char_probs =  nn.Softmax(dim=1)(char_logits).to(device)
+    probs = torch.fmax(probs, 1e-10 * torch.ones_like(probs))
     policy_loss = -(advantages * torch.log(probs)[list(range(len(actions))), actions]).mean()
     if params['char_logit']:
-        entropy_loss = entropy_loss_weight * (-char_probs * torch.log(char_probs)).sum(dim=1).mean()
+        char_probs = nn.Softmax(dim=1)(char_logits).to(device)
+        entropy_loss = entropy_loss_weight * (
+                    -char_probs * torch.log(torch.fmax(char_probs, 1e-10 * torch.ones_like(char_probs)))).sum(
+            dim=1).mean()
     else:
         entropy_loss = entropy_loss_weight * (-probs * torch.log(probs)).sum(dim=1).mean()
-    return policy_loss , entropy_loss
-
-
+    return policy_loss, entropy_loss
