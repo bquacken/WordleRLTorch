17ce096e6b881a5c30cc8a812faaadf5a217190c
bquacken <68025393+bquacken@users.noreply.github.com>
2023-04-06 12:05:15 -0700

    changed eps placement

--- training/losses.py
+++ training/losses.py
@@ -16,14 +16,15 @@ def critic_loss_fn(returns: torch.Tensor, values: torch.Tensor) -> torch.Tensor:
 
 
 def actor_loss_fn(actions_advantages: torch.Tensor,
-                  policy_logits: torch.Tensor, char_logits) -> tuple[torch.Tensor, torch.Tensor]:
+                  policy_logits: torch.Tensor,
+                  char_logits: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     actions = actions_advantages[:, 0].type(torch.LongTensor).to(device)
     advantages = actions_advantages[:, 1].to(device)
-    probs = nn.Softmax(dim=1)(policy_logits).to(device) + eps
-    policy_loss = -(advantages * torch.log(probs)[list(range(len(actions))), actions]).mean()
+    probs = nn.Softmax(dim=1)(policy_logits).to(device)
+    policy_loss = -(advantages * torch.log(probs[list(range(len(actions))), actions] + eps)).mean()
     if params['char_logit']:
-        char_probs = nn.Softmax(dim=1)(char_logits).to(device) + eps
-        entropy_loss = entropy_loss_weight * (-char_probs * torch.log(char_probs)).sum(dim=1).mean()
+        char_probs = nn.Softmax(dim=1)(char_logits).to(device)
+        entropy_loss = entropy_loss_weight * (-char_probs * torch.log(char_probs + eps)).sum(dim=1).mean()
     else:
-        entropy_loss = entropy_loss_weight * (-probs * torch.log(probs)).sum(dim=1).mean()
+        entropy_loss = entropy_loss_weight * (-probs * torch.log(probs + eps)).sum(dim=1).mean()
     return policy_loss, entropy_loss
